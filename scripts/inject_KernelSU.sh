b063d452a98b0faa1dcd601e6aa0e0770fcdca8f
shoey63 <shoey63@ymail.com>
2026-09-19 19:56:22 +0800

    fix: adjust commit filtering for variant injection scripts chore: promote stable channel to top selection do to instability on Github

--- scripts/inject_KernelSU.sh
+++ scripts/inject_KernelSU.sh
@@ -13,7 +13,7 @@ if [ "${USE_DYNAMIC_TRANSPLANT}" == "true" ]; then
     cd ..
     
     cd "${MANAGER_DIR}"
-    UPSTREAM_HASH=$(git log -n 1 --format="%H" -i --grep="ci skip" --grep="skip ci" --invert-grep -- . ":!website/" ":!docs/" ":!scripts/" ":!*.md" ":!.github/")
+    UPSTREAM_HASH=$(git log -n 1 --format="%H" -i --grep="ci skip" --grep="skip ci" --invert-grep -- manager/ kernel/)
     CALCULATED_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0")
     CALCULATED_COUNT=$(git rev-list --count "${UPSTREAM_HASH}")
     UPSTREAM_BRANCH="main"
@@ -79,7 +79,7 @@ else
     
     # FIX 4: Calculate Hash, Count, and Tag starting strictly from the pristine base commit
     set +o pipefail
-    UPSTREAM_HASH=$(git log -n 1 --format="%H" --first-parent "${RAW_BASE}" -i --grep="ci skip" --grep="skip ci" --invert-grep -- . ":!website/" ":!docs/" ":!*.md" ":!.github/" ":!scripts/")
+    UPSTREAM_HASH=$(git log -n 1 --first-parent "${RAW_BASE}" --format="%H" -i --grep="ci skip" --grep="skip ci" --invert-grep -- manager/ kernel/)
     set -o pipefail
     
     CALCULATED_COUNT=$(git rev-list --count "${UPSTREAM_HASH}" 2>/dev/null || echo "11950")
